96a63f2da3de11483d1e98a44b6ddb1b231cfd85
Neil Goldader <Neil Goldader>
2016-01-12 12:43:39 -0500

    Added parsemessagehelp, parseMessage, parseType, parsemessage, parseTimeStamp, and parseString. Some of which are partially functioning.

--- LogAnalysis.hs
+++ LogAnalysis.hs
@@ -2,5 +2,26 @@ module Main where
 
 import           Log
 
+--parseMessage :: String -> LogMessage
+--parseMessage a = testParse
+
+parseMessage :: String -> LogMessage
+parseMessage (c:cs) = parsemessage (words cs)
+
+parseType :: [String] -> MessageType
+parseType (as : cd) = as
+
+parsemessage :: [String] -> ( MessageType, TimeStamp, String )
+parsemessage cd  = (parseType cd, parseTimeStamp cd, parseString cd)
+
+parsemessagehelp :: ( MessageType, TimeStamp, String) -> LogMessage
+parsemessagehelp (m,t,s) = m t s
+
+parseTimeStamp ::
+parseString ((c:as):cd) =
+
+parseString ::
+parseString ((c:as):cd) =
+
 main :: IO ()
 main = undefined
