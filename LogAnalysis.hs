56555e6116dc887476f98b9fa3dd1bbb54307eb0
Barbour <cbarbour@medialab-06.fcs.edu>
2016-01-15 09:12:26 -0500

    messge types added

--- LogAnalysis.hs
+++ LogAnalysis.hs
@@ -4,3 +4,16 @@ import           Log
 
 main :: IO ()
 main = undefined
+
+MaybeMessageType a = Nothing | Just
+
+parseMessageHelper :: [String] -> (Maybe MessageType , String)
+parseMessageHelper x:n:xs = if x == "I"
+						then (Just Info , xs)
+						else if x == "E"
+						then (Just (Error (read n)) , xs) 
+						else if x == "W"
+						then (Just Warning , xs)
+						else Nothing
+
+
