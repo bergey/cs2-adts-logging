ded99d516eff8a7983019b846631c4bb74712238
Woolley <mwoolley@medialab-08.fcs.edu>
2016-01-15 09:09:51 -0500

    started parse error message project look up word and unword

--- LogAnalysis.hs
+++ LogAnalysis.hs
@@ -5,7 +5,14 @@ import           Log
 main :: IO ()
 main = undefined
 
+parseMessagehelper::String->[String]
+parseMessagehelper a = words a
+
+parseMessagehelper2:: [String]->String
+parseMessagehelper2 a = unwords a 
+
 parseMessage :: String -> LogMessage
-parseMessage (a:as) =  parseMessage 
-parseMessage a = 	
-parse :: String -> [LogMessage]
+parseMessage a = LogMessage (untranslatederror!! 0) (untranslatederror!! 1) (untranslatederror!! 2) (parseMessagehelper2 untranslatederror) where
+	untranslatederror = (parseMessagehelper a) 
+
+--parse :: String -> [LogMessage]
