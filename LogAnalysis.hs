3db9226a27d816519f1a378e4bdc3b1ff5972164
Barbour <cbarbour@medialab-06.fcs.edu>
2016-01-21 09:49:10 -0500

    parse messages

--- LogAnalysis.hs
+++ LogAnalysis.hs
@@ -5,15 +5,23 @@ import           Log
 main :: IO ()
 main = undefined
 
-MaybeMessageType a = Nothing | Just
-
-parseMessageHelper :: [String] -> (Maybe MessageType , String)
-parseMessageHelper x:n:xs = if x == "I"
-						then (Just Info , xs)
+parseMessageMessageType :: [String] -> (Maybe MessageType , String)
+parseMessageMessageType (x:n:xs) = if x == "I"
+						then (Just Info , unwords xs) 
 						else if x == "E"
-						then (Just (Error (read n)) , xs) 
+						then (Just (Error (read n)) , unwords xs) 
 						else if x == "W"
-						then (Just Warning , xs)
-						else Nothing
+						then (Just Warning , unwords xs)
+						else (Nothing, unwords xs)
 
+parseMessageTimeStamp :: [String] -> (Maybe MessageType , TimeStamp, String)
+parseMessageTimeStamp (as) = (m, (read w), unwords ws) where
+	(m,xs) = parseMessageMessageType (as)
+	(w:ws) = words xs
 
+parseMessage :: String -> LogMessage
+parseMessage (errorLine) = parseMessageTimeStamp (words errorLine)
+
+parse :: String -> [LogMessage]
+parse (errorlog) = map (parseMessage) (lines errorlog)
+ 
