38c43c31a4e8d1afabdd53914b66c14cc2fa950a
Neil Goldader <Neil Goldader>
2016-01-21 09:44:30 -0500

    Removed bangbang function that was there for debugging.

--- LogAnalysis.hs
+++ LogAnalysis.hs
@@ -40,10 +40,6 @@ parseString cd = if elem (cd !! 2 !! 0 ) ['0','1','2','3','4','5','6','7','8','9
                  then unwords (drop 3 cd)
                  else unwords (drop 2 cd)
 
-bangbang :: [String] -> String
-bangbang cd = if cd !! 0 == "1"
-              then cd !! 0
-              else cd !! 0
 
 main :: IO ()
 main = undefined
