b8888cdd2b27d70f6bd9726a2e3192cceed45690
Barbour <cbarbour@medialab-06.fcs.edu>
2016-01-22 13:18:29 -0500

    Parse Complete

--- LogAnalysis.hs
+++ LogAnalysis.hs
@@ -7,11 +7,11 @@ main = undefined
 
 parseMessageMessageType :: [String] -> (Maybe MessageType , String)
 parseMessageMessageType (x:n:xs) = if x == "I"
-						then (Just Info , unwords xs) 
+						then (Just Info , unwords (n:xs))
 						else if x == "E"
 						then (Just (Error (read n)) , unwords xs) 
 						else if x == "W"
-						then (Just Warning , unwords xs)
+						then (Just Warning , unwords (n:xs))
 						else (Nothing, unwords xs)
 
 parseMessageTimeStamp :: [String] -> (Maybe MessageType , TimeStamp, String)
@@ -20,7 +20,11 @@ parseMessageTimeStamp (as) = (m, (read w), unwords ws) where
 	(w:ws) = words xs
 
 parseMessage :: String -> LogMessage
-parseMessage (errorLine) = parseMessageTimeStamp (words errorLine)
+parseMessage (errorLine) = let (m,t,s) = parseMessageTimeStamp (words errorLine) in 
+	case m of 
+		Nothing -> Unknown errorLine
+		Just mt -> LogMessage mt t s 
+	
 
 parse :: String -> [LogMessage]
 parse (errorlog) = map (parseMessage) (lines errorlog)
