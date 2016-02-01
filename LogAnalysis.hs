e9f7776f8a1f112fbe5ed03655df8a79c1b08192
Woolley <mwoolley@medialab-08.fcs.edu>
2016-02-01 09:21:08 -0500

    commit

--- LogAnalysis.hs
+++ LogAnalysis.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import           Log
+import Log
 
 main :: IO ()
 main = undefined
@@ -11,8 +11,35 @@ parseMessagehelper a = words a
 parseMessagehelper2:: [String]->String
 parseMessagehelper2 a = unwords a 
 
+charToString :: Char -> String
+charToString c = [c]
+
+errormessagetypes::[String]->MessageType 
+errormessagetypes (a:rest) | a!!0 == 'I' = Info
+					| a!!0 == 'W' = Warning
+					| a!!0 == 'E' && b<100 = Error b where
+						b = read (rest!!0)
+
+checkmessagemessagetype::[String]->String
+checkmessagemessagetype (c:cs) | errormessagetypes(c:cs) == Info = cs!!0
+							   | errormessagetypes(c:cs) == Warning = cs!!0   
+							   | otherwise = cs!!1
+
 parseMessage :: String -> LogMessage
-parseMessage a = LogMessage (untranslatederror!! 0) (untranslatederror!! 1) (untranslatederror!! 2) (parseMessagehelper2 untranslatederror) where
+parseMessage a = LogMessage (errormessagetypes(untranslatederror)) (read (checkmessagemessagetype(untranslatederror))) (parseMessagehelper2 (drop 2 untranslatederror)) where
 	untranslatederror = (parseMessagehelper a) 
 
---parse :: String -> [LogMessage]
+parse :: String -> [LogMessage]
+parse a = map parseMessage (lines a)
+
+data MessageTree = Leaf
+| Node MessageTree LogMessage MessageTree
+
+insert :: LogMessage -> MessageTree -> MessageTree
+
+
+
+--build :: [LogMessage] -> MessageTree
+
+--inOrder :: MessageTree -> [LogMessage]
+
