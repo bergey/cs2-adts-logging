1d47d12da0f098deda283c9a52bd572050266b00
Daniel Bergey <bergey@teallabs.org>
2016-02-16 10:34:51 -0500

    add QuickCheck tests, instances

--- Test.hs
+++ Test.hs
@@ -1,8 +1,10 @@
 -- This is how we import only a few definitions from Test.Tasty
 import Test.Tasty (defaultMain, testGroup, TestTree)
 import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
 
 import Log
+import LogInstances
 
 -- import everything *except* `main` from LogAnalysis
 import LogAnalysis hiding (main)
@@ -32,7 +34,7 @@ tests = testGroup "unit tests"
     , testCase "parseMessageMessageType I"
         ( parseMessageMessageType "I 6 Completed armadillo processing"
         @?= (Just Info, "6 Completed armadillo processing"))
-    
+
     -- Add at least 3 more tests for MessageType parsing in isolation.
     , testCase "parseMessageMessageType E"
         ( parseMessageMessageType "E 2 6 Completed armadillo processing"
@@ -41,7 +43,7 @@ tests = testGroup "unit tests"
         ( parseMessageMessageType "W 6 Completed armadillo processing"
         @?= (Just Warning, "6 Completed armadillo processing"))
     , testCase "parseMessage Unknown"
-        ( parseMessageMessageType "Unknown a thing happened" 
+        ( parseMessageMessageType "Unknown a thing happened"
         @?= (Nothing, "a thing happened" ))
     -- Add tests for timestamp parsing.  Think in particular about
     -- what the function does if the input doesn't start with a digit,
@@ -62,25 +64,25 @@ tests = testGroup "unit tests"
         -- I think that for ParseMessage 3 test is enough because I only use 3
         -- functions to parse message. I think that there should be a test for
         -- every function used because it helps you locate the problem in the
-        -- code. 
+        -- code.
     -- Write at least 5 tests for 'insert', with sufficiently
     -- different inputs to test most of the cases.  Look at your code
     -- for 'insert', and any bugs you ran into while writing it.
 
     , testCase "insert info Leaf"
         ( insert (LogMessage Info 6 "Completed armadillo processing"), Leaf
-        @?= (Node Leaf (LogMessage Info 6 "Completed armadillo processing") Leaf )) 
+        @?= (Node Leaf (LogMessage Info 6 "Completed armadillo processing") Leaf ))
 
     , testCase "insert error leaf"
         ( insert (LogMessage (Error 2) 6 "Completed armadillo processing") Leaf
-        @?= (Node Leaf (LogMessage (Error 2) 6 "Completed armadillo processing") Leaf )) 
-    
+        @?= (Node Leaf (LogMessage (Error 2) 6 "Completed armadillo processing") Leaf ))
+
     , testCase "insert info Node"
         ( insert (LogMessage Info 6 "Completed armadillo processing") (Node (Node Leaf (parseMessage "I 2 sdgjh ash") Leaf) (parseMessage "I 5 sdgjh ash") (Node Leaf  (parseMessage "I 13 sdgjh ash") Leaf ))
         @?= (Node (Node Leaf (LogMessage Info 2 "sdgjh ash") Leaf) (LogMessage Info 5 "sdgjh ash") (Node (Node Leaf (LogMessage Info 6 "sdgjh ash") Leaf) (LogMessage Info 13 "sdgjh ash") Leaf)))
 
     , testCase "insert error Node"
-       ( insert (LogMessage (Error 2) 6 "Completed armadillo processing") (Node (Node Leaf (parseMessage "I 2 sdgjh ash") Leaf) (parseMessage "I 5 sdgjh ash") (Node Leaf  (parseMessage "I 13 sdgjh ash") Leaf )) 
+       ( insert (LogMessage (Error 2) 6 "Completed armadillo processing") (Node (Node Leaf (parseMessage "I 2 sdgjh ash") Leaf) (parseMessage "I 5 sdgjh ash") (Node Leaf  (parseMessage "I 13 sdgjh ash") Leaf ))
         @?= (Node (Node Leaf (LogMessage Info 2 "sdgjh ash") Leaf) (LogMessage Info 5 "sdgjh ash") (Node (Node Leaf (LogMessage (Error 2) 6 "sdgjh ash") Leaf) (LogMessage Info 13 "sdgjh ash") Leaf)))
 
     , testCase "instance Ord LogMessage Info"
@@ -88,7 +90,7 @@ tests = testGroup "unit tests"
         @?= ((LogMessage Info 2 "sdgjh ash") < (LogMessage Info 5 "sdgjh ash")))
 
     , testCase "instance Ord LogMessage Error"
-       ( (parseMessage "E 2 2 sdgjh ash") <= (parseMessage "E 5 5 sdgjh ash") 
+       ( (parseMessage "E 2 2 sdgjh ash") <= (parseMessage "E 5 5 sdgjh ash")
         @?= ((LogMessage (Error 2) 2 "sdgjh ash") < (LogMessage (Error 5) 5 "sdgjh ash")))
 
     -- Next week we'll have the computer write more tests, to help us
@@ -104,12 +106,27 @@ tests = testGroup "unit tests"
     -- the file and give them names, to more easiely reuse them.
 
     , testCase "inOrder Empty/Leaf"
-       (inOrder (Leaf) 
+       (inOrder (Leaf)
         @?= [])
     , testCase "inOrder List/Node"
-           ( inOrder (Node (Node Leaf (LogMessage Info 2 "sdgjh ash") Leaf) (LogMessage Info 5 "sdgjh ash") (Node (Node Leaf (LogMessage (Error 2) 6 "sdgjh ash") Leaf) (LogMessage Info 13 "sdgjh ash") Leaf)) 
-        @?= [LogMessage Info 2 "sdgjh ash" , LogMessage Info 5 "sdgjh ash" , LogMessage Error 2 6 "sdgjh ash" , LogMessage Info 13 "sdgjh ash"]) 
+           ( inOrder (Node (Node Leaf (LogMessage Info 2 "sdgjh ash") Leaf) (LogMessage Info 5 "sdgjh ash") (Node (Node Leaf (LogMessage (Error 2) 6 "sdgjh ash") Leaf) (LogMessage Info 13 "sdgjh ash") Leaf))
+        @?= [LogMessage Info 2 "sdgjh ash" , LogMessage Info 5 "sdgjh ash" , LogMessage Error 2 6 "sdgjh ash" , LogMessage Info 13 "sdgjh ash"])
 
+    , testProperty "build sorted"
+    (\msgList -> isSorted (inOrder (build msgList)))
+
+    -- show :: Int -> String
+    -- gives the String representation of an Int
+    -- Use show to test your code to parse Ints
+
+    -- Write a function that takes a MessageType, and makes a String
+    -- with the same format as the log file:
+    -- stringMessageType :: MessageType -> String
+    -- Use this to test your code that parses MessageType
+
+    -- Make another function that makes a String from a whole LogMessage
+    -- stringLogMessage :: LogMessage -> String
+    -- Use it to test parseMessage
   ]
 
 main = defaultMain tests
